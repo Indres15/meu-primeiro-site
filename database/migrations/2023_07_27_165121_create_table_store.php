dffcb077b3090053368f1e3f630b5765c5ca1ba1
fernanda indres <fernanda.indres15@gmail.com>
2023-07-27 17:04:06 -0400

    tabela store

---
+++ database/migrations/2023_07_27_165121_create_table_store.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableStore extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('description');
+            $table->string('phone');
+            $table->string('mobile_phone');
+            $table->string('slug');
+          
+
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
