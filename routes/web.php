79c260848aef6b6d1f81065639f10f96d3f876f6
Fernanda Indres <fernanda.indres15@gmail.com>
2023-07-31 21:06:05 -0400

    buscas do fremework

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,19 @@ Route::get('/model', function() {
 
    // $Products = Product::all(); //select * from products
 
-   $user = new \App\User();
-   $user ->name ='Teste';
-   $user ->email ='email@teste.com';
-   $user ->password = bcrypt('12345678');
+ //  $user = new \App\User();
+ //   $user = \App\User::find(41);
+ //  $user ->name ='Teste';
+ //  $user ->email ='email@teste.com';
+ //  $user ->password = bcrypt('12345678');
 
+ //  $user->save();
     //return $user->save();
+    // \App\User::all(); - retorna todas os Usuarios
+    //  \App\User::find(5); - retorna o usuario com base no id
+    // \App\User::where('name', 'Amaya Grady')->get(); //select * from users where name = Amaya Grady
+    // \App\User::where('name', 'Amaya Grady')->first(); //select * from users where name = Amaya Grady
+    // \App\User::paginate(10); - paginar dados com o laravel
 
-    return \App\User::all();
+      return;
 });
