e708bcd851a441000d048c0296f596c531684879
Fernanda Indres <fernanda.indres15@gmail.com>
2023-08-02 22:02:27 -0400

    Mapeamentos de muitos para muitos (N:N)

---
+++ database/migrations/2023_08_03_013223_create_category_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_product', function (Blueprint $table) {
+           $table->unsignedBigInteger('Product_id');
+           $table->unsignedBigInteger('category_id');
+
+           $table->foreign('Product_id')->references('id')->on('Products');
+           $table->foreign('category_id')->references('id')->on('categorys');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_product');
+    }
+}
