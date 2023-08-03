cab179dceae85cc80a21a32928c245b0c53e8b76
fernanda indres <fernanda.indres15@gmail.com>
2023-08-03 13:28:01 -0400

    create fillable

--- app/Product.php
+++ app/Product.php
@@ -6,8 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = ['name', 'description', 'bory', 'price', 'slug'];
+    
     public function store()
     {
         return $this->belongsTo(store::class);
+    }
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
     }
 }
