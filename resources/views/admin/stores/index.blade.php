1d3d58c7aa046a6ee13ccd67e17d4cc138448535
Fernanda Indres <fernanda.indres15@gmail.com>
2023-08-08 19:42:56 -0400

    criação da blade

---
+++ resources/views/admin/stores/index.blade.php
@@ -0,0 +1,20 @@
+<table>
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>loja</th>
+            <th>Ações</th>
+        </tr>
+    </thead>
+    <body>
+        @foreach ($Stores as $store )
+        <tr>
+            <td>$store = id</td>
+            <td>$store = name</td>
+            <td></td>
+
+        </tr>
+        @endforeach
+        @endforeach
+    </body>
+</table>
