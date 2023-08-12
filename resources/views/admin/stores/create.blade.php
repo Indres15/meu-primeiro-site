871c0705307553a716fd71250d147ef5d43844be
Fernanda Indres <fernanda.indres15@gmail.com>
2023-08-12 16:46:54 -0400

    criação do form-control

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -3,28 +3,33 @@
 @section('content')
     <h1>Criar Loja</h1>
     <form action="/admin/stores/store" method="post">
-        <input type="hidden" name="_token" value="{{csrf_token()}}"> 
-        <div>
+        <input type="hidden" name="_token" value="{{csrf_token()}}">
+        <div class="form-group>
             <label>Nome Loja</label>
-            <input type="text" name="name">
+            <input type="text" name="name" class="form-control">
         </div>
-        <div>
+
+        <div class="form-group>
             <label>Descrição</label>
-            <input type="text" name="description">
+            <input type="text" name="description" class="form-control">
         </div>
-        <div>
+
+        <div class="form-group>
             <label>Telefone</label>
-            <input type="text" name="phone">
+            <input type="text" name="phone" class="form-control">
         </div>
-        <div>
+
+        <div class="form-group>
             <label>Celular/Whatsapp</label>
-            <input type="text" name="mobile_phone">
+            <input type="text" name="mobile_phone" class="form-control">
         </div>
-        <div>
+
+        <div class="form-group>
             <label>Slug</label>
-            <input type="text" name="slug">
+            <input type="text" name="slug" class="form-control">
         </div>
-        <div>
+
+        <div class="form-group>
             <label>Usuário</label>
             <select name="user">
                 @foreach($users as $user)
