ed1bd019869fd245ba8c37893e7e860192f72a13
fernanda indres <fernanda.indres15@gmail.com>
2023-08-14 12:52:13 -0400

    correção

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -4,32 +4,32 @@
     <h1>Criar Loja</h1>
     <form action="/admin/stores/store" method="post">
         <input type="hidden" name="_token" value="{{csrf_token()}}">
-        <div class="form-group>
+        <div class="form-group">
             <label>Nome Loja</label>
             <input type="text" name="name" class="form-control">
         </div>
 
-        <div class="form-group>
+        <div class="form-group">
             <label>Descrição</label>
             <input type="text" name="description" class="form-control">
         </div>
 
-        <div class="form-group>
+        <div class="form-group">
             <label>Telefone</label>
             <input type="text" name="phone" class="form-control">
         </div>
 
-        <div class="form-group>
+        <div class="form-group">
             <label>Celular/Whatsapp</label>
             <input type="text" name="mobile_phone" class="form-control">
         </div>
 
-        <div class="form-group>
+        <div class="form-group">
             <label>Slug</label>
             <input type="text" name="slug" class="form-control">
         </div>
 
-        <div class="form-group>
+        <div class="form-group">
             <label>Usuário</label>
             <select name="user">
                 @foreach($users as $user)
