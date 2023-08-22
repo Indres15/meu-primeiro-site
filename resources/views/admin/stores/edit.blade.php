8b365fb9d8a1364f6e50a886ddcf5f8c448ce0b5
fernanda indres <fernanda.indres15@gmail.com>
2023-08-22 16:59:46 -0400

    correção

--- resources/views/admin/stores/edit.blade.php
+++ resources/views/admin/stores/edit.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <h1>Criar Loja</h1>
-    <form action="{{route('admin.stores.update, ['store'=>$store->id])}}" method="post">
-        <input type="hidden" name="_token" value="{{csrf_token()}}">
+    <form action="{{route('admin.stores.update', ['store'=>$store->id])}}" method="post">
+        @csrf
         @method("PUT")
         
         <div class="form-group">
