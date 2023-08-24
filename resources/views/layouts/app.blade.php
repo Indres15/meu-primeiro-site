2fcf3bf2d8255dd2ed32c3cf4d1fb367f2f252fb
fernanda indres <fernanda.indres15@gmail.com>
2023-08-24 16:56:12 -0400

    controle de exibição do munu admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,29 +18,33 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item @if (request()->is('admin/stores')) active @endif">
-                    <a class="nav-link" href="{{ route('admin.stores.index') }}">Lojas <span
-                            class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item @if (request()->is('admin/products')) active @endif">
-                    <a class="nav-link" href="{{ route('admin.products.index') }}">Produtos</a>
-                </li>
-
-            </ul>
-            <div class="my-2 my-lg-0">
+            @auth
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"
-                            onclick="event.preventDefault(); 
+                    <li class="nav-item @if (request()->is('admin/stores')) active @endif">
+                        <a class="nav-link" href="{{ route('admin.stores.index') }}">Lojas <span
+                                class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item @if (request()->is('admin/products')) active @endif">
+                        <a class="nav-link" href="{{ route('admin.products.index') }}">Produtos</a>
+                    </li>
+
+                </ul>
+
+                <div class="my-2 my-lg-0">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"
+                                onclick="event.preventDefault(); 
                             document.querySelector('form.logout').submit(); ">Sair</a>
 
-                        <form action="{{ route('logout') }}" class="logout" method="post" style="display:none;">
-                            @csrf
-                        </form>
-                    </li>
-                </ul>
-            </div>
+                            <form action="{{ route('logout') }}" class="logout" method="post" style="display:none;">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            @endauth
+
         </div>
     </nav>
     <div class="container">
