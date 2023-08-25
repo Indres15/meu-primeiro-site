e374f5645a912edb213a82c006485b0b9f39e6a6
fernanda indres <fernanda.indres15@gmail.com>
2023-08-25 11:24:43 -0400

    exibição do nome do usuario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,10 @@
                                 @csrf
                             </form>
                         </li>
+                        <li class="nav-item">
+                            <span class="nav-link">{{ auth()->user()->name }}</span>
+
+                        </li>
                     </ul>
                 </div>
             @endauth
