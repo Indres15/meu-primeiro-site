2cbedf053c98c3f7d6231a8e338b5ada6bdb929f
fernanda indres <fernanda.indres15@gmail.com>
2023-08-28 17:30:57 -0400

    controle de loja por usuario

--- app/Http/Controllers/Admin/StoreController.php
+++ app/Http/Controllers/Admin/StoreController.php
@@ -22,13 +22,19 @@ class StoreController extends Controller
 
     public function index()
     {
-        $stores = \App\Store::paginate(10);
+        $store = auth()->user()->store;
 
-        return view('admin.stores.index', compact('stores'));
+        return view('admin.stores.index', compact('store'));
     }
 
     public function create()
     {
+        if(auth()->user()->store()->count()) {
+            flash('Você já possui uma loja') warning();
+            return redirect()->route('admin.stores.index');
+
+        }
+
         $users = \App\User::all(['id', 'name']);
 
         return view('admin.stores.create', compact('users'));
--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -2,36 +2,38 @@
 
 
 @section('content')
-<a href="{{route('admin.stores.create')}}" class="btn btn-lg btn-success">Criar Loja</a>
+    @if (!store)
+        <a href="{{ route('admin.stores.create') }}" class="btn btn-lg btn-success">Criar Loja</a>
+    @endif
 
-<table class='table table-striped'>
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>loja</th>
-            <th>Ações</th>
-        </tr>
-    </thead>
-    <body>
-        @foreach ($stores as $store )
-        <tr>
-            <td>{{$store->id}}</td>
-            <td>{{$store->name}}</td>
-            <td>
-                <div class="btn-group">
+    <table class='table table-striped'>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>loja</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
 
-                    <a href="{{route('admin.stores.edit', ['store' => $store->id])}}" class="btn btn-sm btn-outline-primary">EDITAR</a>   
-                    <form action="{{route('admin.stores.destroy', ['store' => $store->id])}}" method="post">
-                         @csrf
-                         @method('DELETE')
-                         <button type="submit" class="btn btn-sm btn-outline-danger">REMOVER</button> 
-                     </form>
-                </div>
-            </td> 
-        </tr>
-        @endforeach
-    </body>
-</table>
+        <body>
 
-{{$stores->links()}}
+            <tr>
+                <td>{{ $store->id }}</td>
+                <td>{{ $store->name }}</td>
+                <td>
+                    <div class="btn-group">
+
+                        <a href="{{ route('admin.stores.edit', ['store' => $store->id]) }}"
+                            class="btn btn-sm btn-outline-primary">EDITAR</a>
+                        <form action="{{ route('admin.stores.destroy', ['store' => $store->id]) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-sm btn-outline-danger">REMOVER</button>
+                        </form>
+                    </div>
+                </td>
+            </tr>
+
+        </body>
+    </table>
 @endsection
