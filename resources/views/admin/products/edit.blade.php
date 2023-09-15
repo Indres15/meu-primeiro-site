a44d111306fc6b627cdbf77e8ddc333f9ae7689b
fernanda indres <fernanda.indres15@gmail.com>
2023-09-15 12:30:05 -0400

    upload de foto

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -54,8 +54,14 @@
 
     <div class="row">
         @foreach ($product->photos as $photo)
-            <div class="col-4">
+            <div class="col-4 text-center">
                 <img src="{{ asset('storage/' . $photo->image) }}" alt="" class="img-fluid">
+                <form action="{{ route('admin.photo.remove') }}" method="post">
+                    <input type="hidden" name="photoName" value="{{ $photo->image }}">
+                    @csrf
+                    <button class="btn btn-sm btn-outline-danger">REMOVER</button>
+                
+                </form>
             </div>
         @endforeach
     </div>
