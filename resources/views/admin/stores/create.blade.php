ca0cb8040d135f9c8c3668927c1f724a6a7e3fd9
fernanda indres <fernanda.indres15@gmail.com>
2023-09-21 13:06:13 -0400

    valideação de upload

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -54,7 +54,13 @@
 
         <div class="form-group">
             <label>Fotos do Produto</label>
-            <input type="file" name="logo" class="form-control">
+            <input type="file" name="logo" class="form-control @error('logo') is-invalid @enderror">
+
+            @error('logo')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
