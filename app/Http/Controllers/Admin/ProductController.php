665eff8d0cf4501d83795245968f0f456ac32794
fernanda indres <fernanda.indres15@gmail.com>
2023-09-25 13:15:10 -0400

    correção de sintax

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -53,11 +53,12 @@ class ProductController extends Controller
     public function store(ProductRequest $request)
     {
         $data = $request->all();
+        $cateegories = $request->get('categories', null);
 
         $store = User::find(auth()->id())->store;
         $product = $store->products()->create($data);
 
-        $product->categories()->sync($data['categories']);
+        $product->categories()->sync($cateegories);
 
         if($request->hasFile('photos')) {
             $images = $this->imageUpload($request->file('photos'), 'image');
@@ -103,10 +104,13 @@ class ProductController extends Controller
     public function update(ProductRequest $request, $product)
     {
         $data = $request->all();
+        $categories = $request->get('categories', null);
 
         $product = $this->product->find($product);
         $product->update($data);
-        $product->categories()->sync($data['categories']);
+
+        if(!is_null($categories))
+            $product->categories()->sync($categories);
 
         if($request->hasFile('photos')) {
             $images = $this->imageUpload($request->file('photos'), 'image');
