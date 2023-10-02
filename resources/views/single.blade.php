b0ffcacf1a26da80f8dc05a203803c9d10f9fe1d
fernanda indres <fernanda.indres15@gmail.com>
2023-10-02 17:23:40 -0400

    correções

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-6">
+        <div class="col-4">
             @if ($product->photos->count())
                 <img src="{{ asset('storage/' . $product->photos->first()->image) }}" alt="" class="card-img-top">
                 <div class="row" style="margin-top: 20px;">
@@ -17,18 +17,35 @@
                 <img src="{{ asset('assets/img/no-photo.jpg') }}" alt="" class="card-img-top"> 
             @endif
         </div>
-        <div class="col-8">
-            <h2>{{ $product->name }}</h2>
-            <p>
-                {{ $product->description }}
-            </p>
-            <h3>
-               R$ {{number_format($product->price, '2', ',', '.') }}
-            </h3>
+        <div class="col-6">
+            <div class="col-md-12">
 
-            <span>
-                Loja: {{ $product->store->name }}
-            </span>
+                <h2>{{ $product->name }}</h2>
+
+                <p>
+                    {{ $product->description }}
+                </p>
+
+                <h3>
+                R$ {{number_format($product->price, '2', ',', '.') }}
+                </h3>
+
+                <span>
+                    Loja: {{ $product->store->name }}
+                </span>
+            </div>
+
+            <div class="product-add col-md-12">
+                <hr>
+                
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label>Quantidade</label>
+                        <input type="number" class="form-control col-md-2">
+                    </div>
+                    <button class="btn btn-lg btn-danger">Comprar</button>
+                </form>
+            </div>
         </div>
     </div>
 
