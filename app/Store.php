dd589e87ff5ab5ccbabbf1c0c2181c71725c727f
fernanda indres <fernanda.indres15@gmail.com>
2023-11-06 13:20:39 -0400

    tableça pedido usuario

--- app/Store.php
+++ app/Store.php
@@ -31,4 +31,10 @@ class Store extends Model
     {
         return $this->hasMany(Product::class);
     }
+
+    public function orders()
+    {
+        return $this->hasMany(UserOrder::class);
+
+    }
 }
