7793086c5de3b2bfe2b3eaa5c1f5f16bcbe7f5d7
fernanda indres <fernanda.indres15@gmail.com>
2023-11-06 13:20:01 -0400

    migração tabela de pedidos do usuario

---
+++ app/UserOrder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserOrder extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+
+    }
+
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+}
+
