2082cb79d7685d16fab014032b42528a6c1fe114
Fernanda Indres <fernanda.indres15@gmail.com>
2023-11-23 16:17:06 -0400

    bloq da manipulação do valor/descrição da inspeção

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,56 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\Cast\Array_;
 
 class CartController extends Controller
 {
     public function index()
     {
-       
+
 
         $cart = session()->has('cart') ? session()->get('cart') : [];
 
         return view('cart', compact('cart'));
     }
-     
+
     public function add(Request $request)
     {
-        $product = $request->get('product');
+        $productData = $request->get('product');
 
-        if(session()->has('cart')) {
+        $product = \App\Product::whereSlug($productData['slug']);
+
+        if (!$product->count() || $productData['amount'] == 0)
+            return redirect()->route('home');
+
+        $product =
+            $product = array_merge(
+                $productData,
+                $product->first(['name', 'price'])->toArray()
+            );
+
+        if (session()->has('cart')) {
 
             $products = session()->get('cart');
             $productsSlugs = array_column($products, 'slug');
 
-            if(in_array($product['slug'], $productsSlugs)) {
+            if (in_array($product['slug'], $productsSlugs)) {
                 $products = $this->productIncrement($product['slug'], $product['amount'], $products);
 
                 session()->put('cart', $products);
-
-            } else{
+            } else {
 
                 session()->push('cart', $product);
             }
-
         } else {
 
             $products[] = $product;
 
             session()->put('cart', $products);
-
         }
         flash('Produto Adicionado no Carrinho!')->success();
         return redirect()->route('product.single', ['slug' => $product['slug']]);
     }
     public function remove($slug)
     {
-        if(!session()->has('cart'))
-        return redirect()->route('cart');
+        if (!session()->has('cart'))
+            return redirect()->route('cart');
 
         $products = session()->get('cart');
 
-        $products = array_filter($products, function($line) use($slug){
+        $products = array_filter($products, function ($line) use ($slug) {
             return $line['slug'] != $slug;
         });
 
@@ -68,9 +78,9 @@ class CartController extends Controller
 
     private function productIncrement($slug, $amount, $products)
     {
-        $products = array_map(function($line) use($slug, $amount){
+        $products = array_map(function ($line) use ($slug, $amount) {
 
-            if($slug == $line['slug']){
+            if ($slug == $line['slug']) {
                 $line['amount'] += $amount;
             }
             return $line;
